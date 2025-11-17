8373d68363542c0ad71f15215f29e7495affdf2d
PranavKoduriOSU <pranavkoduriosu@outlook.com>
2025-11-16 21:20:04 -0600

    updated resource buildings to include id of their corresponding resource

--- src/main/java/Models/Buildings/ResourceCollector.java
+++ src/main/java/Models/Buildings/ResourceCollector.java
@@ -5,16 +5,22 @@ import Util.Models.Upgrade;
 
 public class ResourceCollector extends Building {
     int genRate;
+    int resGenID;
 
     public ResourceCollector(int buildingID, int buildingTypeID, String buildingName, int resourceID, String resourceName, int maxHealth,
-               int buildingLevel, Upgrade upgradeInfo, boolean currentlyUpgrading, int generatedPerHour){
+               int buildingLevel, Upgrade upgradeInfo, boolean currentlyUpgrading, int generatedPerHour, int resourceGeneratedID){
         super(buildingID, buildingTypeID, buildingName, resourceID, resourceName, maxHealth, buildingLevel, upgradeInfo, currentlyUpgrading);
         genRate = generatedPerHour;
+        resGenID = resourceGeneratedID;
     }
 
     @Override
     public void upgrade(){
         super.upgrade();
+    }
+
+    public int GetResourceGeneratedID(){
+        return resGenID;
     }
 
     public int GetResourceGenerationRate(){
--- src/main/java/Models/Buildings/ResourceStorage.java
+++ src/main/java/Models/Buildings/ResourceStorage.java
@@ -5,16 +5,22 @@ import Util.Models.Upgrade;
 
 public class ResourceStorage extends Building {
     int maxCap;
+    int resStoreID;
 
     public ResourceStorage(int buildingID, int buildingTypeID, String buildingName, int resourceID, String resourceName, int maxHealth,
-                             int buildingLevel, Upgrade upgradeInfo, boolean currentlyUpgrading, int maxCapacity){
+                             int buildingLevel, Upgrade upgradeInfo, boolean currentlyUpgrading, int maxCapacity, int resourceStoredID){
         super(buildingID, buildingTypeID, buildingName, resourceID, resourceName, maxHealth, buildingLevel, upgradeInfo, currentlyUpgrading);
         maxCap = maxCapacity;
+        resStoreID = resourceStoredID;
     }
 
     @Override
     public void upgrade(){
         super.upgrade();
+    }
+
+    public int GetResourceGeneratedID(){
+        return resStoreID;
     }
 
     public int GetMaxCapacity(){
