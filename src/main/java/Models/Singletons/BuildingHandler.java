0d13a8f911ac4474aefabee2c6b0f2de6316a295
PranavKoduriOSU <pranavkoduriosu@outlook.com>
2025-11-19 15:27:08 -0600

    quick building handler change

--- src/main/java/Models/Singletons/BuildingHandler.java
+++ src/main/java/Models/Singletons/BuildingHandler.java
@@ -9,17 +9,18 @@ public class BuildingHandler {
     private BuildingHandler() {}
     public static BuildingHandler getInstance() {return handler;}
 
-    private Map<Integer, String> buildingTypes; // building type ID -> building type name
+    private static Map<Integer, String> buildingTypes; // building type ID -> building type name
+
     private int numBuildings = 0;
 
     public static void GenerateValues(){
-        handler.buildingTypes = Game.getInstance().GetRequirementService().GetBuildingTypes();
+        buildingTypes = Game.getInstance().GetRequirementService().GetBuildingTypes();
     }
     public static void LoadBuildings(int profID){
 
     }
 
-    public String GetBuildingTypeName(int buildingTypeID){
+    public static String GetBuildingTypeName(int buildingTypeID){
         return buildingTypes.get(buildingTypeID);
     }
 }
